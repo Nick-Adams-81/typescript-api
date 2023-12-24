da267ad9989c44e9c4a56ad521c1e675bb4b1621
Nicholas Adams <nicholasadams@Owners-MBP.attlocal.net>
2023-12-24 14:42:00 -0800

    mongo connection

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,33 @@
-console.log("hello typescript!");
+const express = require("express");
+const http = require("http");
+const bodyParser = require("body-parser");
+const cookieParser = require("cookie-parser");
+const compression = require("compression");
+const cors = require("cors");
+const mongoose = require("mongoose");
+require("dotenv").config();
+
+const app = express();
+app.use(
+  cors({
+    credentials: true,
+  })
+);
+
+app.use(compression());
+app.use(bodyParser());
+app.use(cookieParser());
+
+const server = http.createServer(app);
+
+server.listen(8080, () =>
+  console.log("Server running on http://localhost:8080/")
+);
+
+const creds = process.env.MONGO_URI;
+mongoose.Promise = Promise;
+mongoose.connect(creds, {useNewUrlParser: true});
+mongoose.connection.on("error", (error: Error) => console.log(error));
+
+// mongo user: user
+// mongo password: 59lhW9RFO3rbTdQq
